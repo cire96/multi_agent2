ee12ce05c01ee21d454c755ffe83f0d4946e125d
Manuel Rickli <manuel.rickli@stud.unibas.ch>
2020-03-22 19:15:36 +0100

    p5 simple path planning

--- Assignment_2/Assets/Scrips/CarAI5.cs
+++ Assignment_2/Assets/Scrips/CarAI5.cs
@@ -14,16 +14,26 @@ namespace UnityStandardAssets.Vehicles.Car {
         public GameObject[] friends;
         public GameObject[] enemies;
 
-        private int SQUARE_SIZE = 5;
+        private int SQUARE_SIZE = 8;
         private float[, ] newTerrain;
         private float[, , ] vision;
         private List<Node> leafs;
         private float stepx;
         private float stepz;
         private TerrainInfo terrainInfo;
-        private Vector3 target;
+        private bool backing = false;
+        private int counter;
+        private Node bestTarget;
+        private Vector3 lastPoint;
+        private List<Vector3> visited;
+        private int enemySize;
+        private bool gotTarget;
+        private Node target;
 
         private void Start () {
+            if (SQUARE_SIZE % 2 != 1) {
+                print ("SQUARE_SIZE must be an odd number!");
+            }
             // get the car controller
             m_Car = GetComponent<CarController> ();
             terrain_manager = terrain_manager_game_object.GetComponent<TerrainManager> ();
@@ -49,33 +59,21 @@ namespace UnityStandardAssets.Vehicles.Car {
                 }
             }
 
-            vision = new float[enemies.Length, newTerrain.GetLength (0), newTerrain.GetLength (1)];
-            int enemy_nr = 0;
-            foreach (GameObject enemy in enemies) {
-                for (int i = 0; i < newTerrain.GetLength (0); i++) {
-                    float posx = terrainInfo.x_low + stepx / 2 + stepx * i;
-                    for (int j = 0; j < newTerrain.GetLength (1); j++) {
-                        float posz = terrainInfo.z_low + stepz / 2 + stepz * j;
+            enemySize = enemies.Length;
 
-                        Vector3 destination = new Vector3 (posx, 0, posz);
-
-                        float distance = (enemy.transform.position - destination).magnitude;
-                        Vector3 direction = (enemy.transform.position - destination).normalized;
-                        int layerMask = 1 << 9;
-
-                        if (Physics.Linecast (enemy.transform.position, destination, layerMask)) {
-                            vision[enemy_nr, i, j] = 0;
-                        } else {
-                            vision[enemy_nr, i, j] = 1;
-                        }
-
-                    }
-                }
-                enemy_nr++;
-            }
+            initVision ();
 
             leafs = getSuccessors (null);
-            target = getAvgPos ();
+            target = null;
+
+            gotTarget = false;
+            lastPoint = getAvgPos ();
+
+            visited = new List<Vector3> ();
+            visited.Add (getSquare (lastPoint.x, lastPoint.z).getPosition ());
+
+            // use this to visualize the heat map
+            //------------------------------------
 
             // float[, ] maps = sumUpHeatMap (vision);
 
@@ -125,33 +123,203 @@ namespace UnityStandardAssets.Vehicles.Car {
             //         }
             //     }
             // }
+        }
 
+        // creates a 2d array with integers which tell how many turrets have vision to which cells on the map
+        private void initVision () {
+            vision = new float[enemySize, newTerrain.GetLength (0), newTerrain.GetLength (1)];
+            int enemy_nr = 0;
+            foreach (GameObject enemy in enemies) {
+                if (enemy != null) {
+                    for (int i = 0; i < newTerrain.GetLength (0); i++) {
+                        float posx = terrainInfo.x_low + stepx / 2 + stepx * i;
+                        for (int j = 0; j < newTerrain.GetLength (1); j++) {
+                            float posz = terrainInfo.z_low + stepz / 2 + stepz * j;
+
+                            Vector3 destination = new Vector3 (posx, 0, posz);
+
+                            float distance = (enemy.transform.position - destination).magnitude;
+                            Vector3 direction = (enemy.transform.position - destination).normalized;
+                            int layerMask = 1 << 9;
+
+                            if (Physics.Linecast (enemy.transform.position, destination, layerMask)) {
+                                vision[enemy_nr, i, j] = 0;
+                            } else {
+                                vision[enemy_nr, i, j] = 1;
+                            }
+
+                        }
+                    }
+                    enemy_nr++;
+                }
+            }
+        }
+
+        // recursively gives a target point which points to a square where at least one turret can be seen
+        private Node getTarget () {
+
+            float bestCost = int.MaxValue;
+            bestTarget = null;
+
+            // if a square with vision from at least half of the cells is found, take it
+            foreach (Node leaf in leafs) {
+                if (visited.Contains (leaf.getSquare ().getPosition ())) {
+                    continue;
+                }
+
+                float cost = leaf.getCost ();
+                if (cost > (SQUARE_SIZE * SQUARE_SIZE) / 2 && cost < bestCost) {
+                    bestCost = cost;
+                    bestTarget = leaf;
+                }
+            }
+
+            if (bestTarget == null) {
+                float bestDistance = 0;
+
+                // get the successor with max distance to where you've just been and call getTarget() again with updated leafs
+                foreach (Node leaf in leafs) {
+                    if (visited.Contains (leaf.getSquare ().getPosition ())) {
+                        continue;
+                    }
+
+                    float distance = Vector3.Distance (leaf.getSquare ().getPosition (), lastPoint);
+                    if (distance > bestDistance) {
+                        bestTarget = leaf;
+                        bestDistance = distance;
+                    }
+                }
+
+                visited.Add (bestTarget.getSquare ().getPosition ());
+                leafs = null;
+                leafs = getSuccessors (bestTarget);
+
+                return getTarget ();
+            } else {
+                visited.Add (bestTarget.getSquare ().getPosition ());
+                leafs = null;
+                leafs = getSuccessors (bestTarget);
+                return bestTarget;
+            }
+        }
+
+        // check if an enemy has been destroyed -> implies need for new vision map
+        private bool checkChange () {
+            int counter = 0;
+            foreach (GameObject g in enemies) {
+                if (g != null) {
+                    counter++;
+                }
+            }
+
+            if (counter != enemySize) {
+                enemySize = counter;
+                return true;
+            } else {
+                return false;
+            }
         }
 
         private void FixedUpdate () {
-            foreach (Node leaf in leafs) {
-                if (leaf.getCost () == SQUARE_SIZE) {
-                    target = leaf.getSquare ().getPosition ();
-                }
-
-                Debug.DrawLine (getAvgPos (), leaf.getSquare ().getPosition ());
+            // when a car is destroyed
+            if (m_Car.transform == null) {
+                return;
             }
 
-            print (leafs.Count);
+            // if a turret is destroyed, initialize the vision map again and reset the visited list -> "start from new"
+            if (checkChange ()) {
+                initVision ();
+                lastPoint = getAvgPos ();
+                gotTarget = false;
+                visited = null;
+                visited = new List<Vector3> ();
+                visited.Add (getSquare (lastPoint.x, lastPoint.z).getPosition ());
+            }
 
-            m_Car.Move (0f, 1f, 1f, 0f);
+            if (!gotTarget) {
+                leafs = null;
+                leafs = getSuccessors (null);
+
+                target = getTarget ();
+                if (target != null) {
+                    gotTarget = true;
+                } else {
+                    return;
+                }
+            } else {
+                if (Vector3.Distance (getAvgPos (), target.getSquare ().getPosition ()) < 7) {
+                    gotTarget = false;
+                    return;
+                }
+            }
+
+            Debug.DrawLine (getAvgPos (), target.getSquare ().getPosition ());
+
+            Vector3 carToTarget = m_Car.transform.InverseTransformPoint (target.getSquare ().getPosition ());
+            float newSteer = (carToTarget.x / carToTarget.magnitude);
+            float newSpeed = 1f; //(carToTarget.z / carToTarget.magnitude);
+
+            float infrontOrbehind = (carToTarget.z / carToTarget.magnitude);
+            if (infrontOrbehind < 0) {
+                newSpeed = -1;
+                if (newSteer < 0) {
+                    newSteer = 1;
+                } else {
+                    newSteer = -1;
+                }
+            } else { newSpeed = 1f; }
+            //if(infrontOrbehind<0 && Mathf.Abs(newSteer)<0.1){newSteer =1;}
+            float handBreak = 0f;
+
+            Vector3 steeringPoint = (transform.rotation * new Vector3 (0, 0, 1));
+            RaycastHit rayHit;
+            LayerMask mask = LayerMask.GetMask ("CubeWalls");
+            //bool hitBack = body.SweepTest(steeringPoint,out rayHit, 2.0f);
+            //bool hitContinue = body.SweepTest(steeringPoint,out rayHit, 8.0f);
+            bool hitBack = Physics.SphereCast (transform.position, 3.0f, steeringPoint, out rayHit, 3.0f, mask);
+            bool hitForward = Physics.SphereCast (transform.position, 3.0f, -steeringPoint, out rayHit, 2.5f, mask);
+            Debug.DrawRay (transform.position, steeringPoint * 5.0f, Color.cyan, 0.1f);
+            Debug.DrawRay (transform.position, -steeringPoint * 5.0f, Color.red, 0.1f);
+            bool hitContinue = Physics.SphereCast (transform.position, 3.0f, steeringPoint, out rayHit, 12.0f, mask);
+            if (hitBack) {
+                backing = true;
+                newSpeed = -1f;
+                if (m_Car.BrakeInput > 0 && m_Car.AccelInput <= 0) {
+                    newSteer = -newSteer;
+                }
+                print ("back");
+
+            }
+
+            if (hitContinue && backing == true) {
+                newSpeed = -1f;
+                newSteer = -newSteer;
+                print ("continue");
+            } else {
+                backing = false;
+            }
+            if (hitForward) {
+                newSpeed = 1;
+            }
+
+            Debug.DrawLine (transform.position, target.getSquare ().getPosition ());
+
+            m_Car.Move (newSteer, newSpeed, newSpeed, 0f);
         }
 
         private Vector3 getAvgPos () {
             Vector3 result = new Vector3 ();
-
+            int counter = 0;
             foreach (GameObject g in friends) {
-                result.x += g.transform.position.x;
-                result.z += g.transform.position.z;
+                if (g != null && g.transform != null) {
+                    result.x += g.transform.position.x;
+                    result.z += g.transform.position.z;
+                    counter++;
+                }
             }
 
-            result.x /= friends.Length;
-            result.z /= friends.Length;
+            result.x /= counter;
+            result.z /= counter;
 
             return result;
         }
@@ -164,6 +332,7 @@ namespace UnityStandardAssets.Vehicles.Car {
             return (int) ((posz - terrainInfo.z_low) / stepz);
         }
 
+        // get a 2d array with the summed up values of all enemies in the heat map
         private float[, ] sumUpHeatMap (float[, , ] maps) {
             float[, ] result = new float[maps.GetLength (1), maps.GetLength (2)];
 
@@ -178,25 +347,23 @@ namespace UnityStandardAssets.Vehicles.Car {
             return result;
         }
 
+        // create a square at the given position
+        // return null if the square lies outside the map or if the position given lies within an object
         private Square getSquare (float x, float z) {
             int posx = getIIndex (x);
             int posz = getJIndex (z);
+            int half = (SQUARE_SIZE - 1) / 2;
 
-            if (posx < 0 || posx > newTerrain.GetLength (0) - 1 || posz < 0 || posz > newTerrain.GetLength (1) - 1) {
+            if (posx < 0 || posx > newTerrain.GetLength (0) - 1 - half || posz < 0 || posz > newTerrain.GetLength (1) - 1 - half || newTerrain[posx, posz] != 0) {
                 return null;
             }
 
-            Square square = new Square (new float[SQUARE_SIZE, SQUARE_SIZE], x, z);
-            int half = (SQUARE_SIZE - 1) / 2;
+            Square square = new Square (new float[SQUARE_SIZE, SQUARE_SIZE], terrainInfo.x_low + (posx * stepx), terrainInfo.z_low + (posz * stepz));
             float[, ] heatMap = sumUpHeatMap (vision);
 
             for (int i = SQUARE_SIZE - 1; i >= 0; i--) {
                 for (int j = SQUARE_SIZE - 1; j >= 0; j--) {
-                    if (newTerrain[posx + i - half, posz + j - half] == 0) {
-                        square.setSquare (i, j, heatMap[posx + i - half, posz + j - half]);
-                    } else {
-                        return null;
-                    }
+                    square.setSquare (i, j, heatMap[posx + i - half, posz + j - half]);
                 }
             }
 
@@ -204,6 +371,16 @@ namespace UnityStandardAssets.Vehicles.Car {
             return square;
         }
 
+        private void drawSquare (Square square) {
+
+            GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+            Collider col = cube.GetComponent<Collider> ();
+            col.enabled = false;
+            cube.transform.localScale = new Vector3 (1f, 0.5f, 1f);
+            cube.transform.position = square.getPosition ();
+        }
+
+        // sums up the values of all cells in the given square
         private float cost (Square square) {
             float sum = 0;
             for (int i = 0; i < square.getSquare ().GetLength (0); i++) {
@@ -215,6 +392,7 @@ namespace UnityStandardAssets.Vehicles.Car {
             return sum;
         }
 
+        // returns all possible successors from the given parent node
         private List<Node> getSuccessors (Node parent) {
             List<Node> result = new List<Node> ();
             Node node;
@@ -234,56 +412,56 @@ namespace UnityStandardAssets.Vehicles.Car {
                 parent_cost = parent.getSquare ().getCost ();
             }
 
-            Square square = getSquare (posx - SQUARE_SIZE, posz - SQUARE_SIZE);
+            Square square = getSquare (posx - (SQUARE_SIZE * stepx), posz - (SQUARE_SIZE * stepz));
             if (square != null) {
                 square.increaseCost (parent_cost);
                 node = new Node (square, parent);
                 result.Add (node);
             }
 
-            square = getSquare (posx, posz - SQUARE_SIZE);
+            square = getSquare (posx, posz - (SQUARE_SIZE * stepz));
             if (square != null) {
                 square.increaseCost (parent_cost);
                 node = new Node (square, parent);
                 result.Add (node);
             }
 
-            square = getSquare (posx + SQUARE_SIZE, posz - SQUARE_SIZE);
+            square = getSquare (posx + (SQUARE_SIZE * stepx), posz - (SQUARE_SIZE * stepz));
             if (square != null) {
                 square.increaseCost (parent_cost);
                 node = new Node (square, parent);
                 result.Add (node);
             }
 
-            square = getSquare (posx - SQUARE_SIZE, posz);
+            square = getSquare (posx - (SQUARE_SIZE * stepx), posz);
             if (square != null) {
                 square.increaseCost (parent_cost);
                 node = new Node (square, parent);
                 result.Add (node);
             }
 
-            square = getSquare (posx + SQUARE_SIZE, posz);
+            square = getSquare (posx + (SQUARE_SIZE * stepx), posz);
             if (square != null) {
                 square.increaseCost (parent_cost);
                 node = new Node (square, parent);
                 result.Add (node);
             }
 
-            square = getSquare (posx - SQUARE_SIZE, posz + SQUARE_SIZE);
+            square = getSquare (posx - (SQUARE_SIZE * stepx), posz + (SQUARE_SIZE * stepz));
             if (square != null) {
                 square.increaseCost (parent_cost);
                 node = new Node (square, parent);
                 result.Add (node);
             }
 
-            square = getSquare (posx, posz + SQUARE_SIZE);
+            square = getSquare (posx, posz + (SQUARE_SIZE * stepz));
             if (square != null) {
                 square.increaseCost (parent_cost);
                 node = new Node (square, parent);
                 result.Add (node);
             }
 
-            square = getSquare (posx + SQUARE_SIZE, posz + SQUARE_SIZE);
+            square = getSquare (posx + (SQUARE_SIZE * stepx), posz + (SQUARE_SIZE * stepz));
             if (square != null) {
                 square.increaseCost (parent_cost);
                 node = new Node (square, parent);
@@ -326,7 +504,7 @@ namespace UnityStandardAssets.Vehicles.Car {
             }
 
             public Vector3 getPosition () {
-                return new Vector3 (this.posx, this.posz);
+                return new Vector3 (this.posx, 0, this.posz);
             }
         }
 
@@ -341,6 +519,10 @@ namespace UnityStandardAssets.Vehicles.Car {
 
             public float getCost () {
                 return this.square.getCost ();
+            }
+
+            public void setCost (float cost) {
+                this.square.setCost (cost);
             }
 
             public Square getSquare () {
